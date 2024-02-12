e7c59fd3d58382466fb7d97b16d433bf833561a2
coderyu5523 <coderyu5523@naver.com>
2024-02-12 20:36:40 +0900

    페이징

--- src/main/java/com/example/springblog/board/Boardcontroller.java
+++ src/main/java/com/example/springblog/board/Boardcontroller.java
@@ -1,11 +1,45 @@
 package com.example.springblog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class Boardcontroller {
+
+    private final BoardRepository boardRepository;
+
     @GetMapping({ "/", "/board" })
-    public String index() {
+    public String index(HttpServletRequest request, @RequestParam(defaultValue = "0") int page) {
+
+        List<Board> boardList = boardRepository.findAll(page);
+        request.setAttribute("boardList",boardList);
+
+        int currentPage = page ;
+        int nextPage = currentPage +1;
+        int prevPage = currentPage -1;
+
+        request.setAttribute("nextPage",nextPage);
+        request.setAttribute("prevPage",prevPage);
+        int totalCount = 4 ;
+
+        boolean first = (currentPage==0?true:false);
+        request.setAttribute("first",first);
+        int totalPage = totalCount/3;
+        if(totalCount%3==0){
+            int lastPage = totalPage -1;
+            boolean last = (currentPage==lastPage?true:false);
+            request.setAttribute("last",last);
+        }else if(totalCount%3!=0){
+            int lastPage = totalPage ;
+            boolean last = (currentPage==lastPage?true:false);
+            request.setAttribute("last",last);
+        }
 
         return "index";
     }
